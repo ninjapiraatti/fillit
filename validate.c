7658706f0ce2b49170834a56dc0853063aac78d4
Tuomas Louekari <tlouekar@c1r5p17.hive.fi>
2019-12-10 14:15:56 +0200

    Validation may be working

--- validate.c
+++ validate.c
@@ -32,14 +32,14 @@ int		validate_tetrimino(int start, char *raw)
 	ncount = 0;
 	while (i < 16)
 	{
-		if ((raw[start] == '#') && ((start + 1) % 16 != 0))
+		if ((raw[start + i] == '#') && ((start + i + 1) % 16 != 0))
 		{
-			if ((raw[start + 4] == '#') && ((start + 1) % 16 < 13))
+			if ((raw[start + i + 4] == '#') && ((start + i + 1) % 16 < 13))
 			{
 				//printf("i and modulo: %d, %d\n", i, ((i + 1) % 16));
 				ncount += 2;
 			}
-			if (raw[start + 1] == '#')
+			if (raw[start + i + 1] == '#')
 			{
 				ncount += 2;
 				//printf("i: %d\n", i);
@@ -52,13 +52,16 @@ int		validate_tetrimino(int start, char *raw)
 				printf("i: %d\n", i);
 			}
 			*/
-			printf("i and ncount: %d, %d\n", i + 1, ncount);
 		}
-		start++;
+		i++;
 	}
-	if (ncount != 6 || ncount != 8)
-		return (0);
-	return (1);
+	if (ncount == 6 || ncount == 8)
+	{
+		printf("VALID at ncount: %d\n", ncount);
+		return (1);
+	}
+	printf("INVALID at ncount: %d\n", ncount);
+	return (0);
 }
 
 int		validate_nbrs(char *raw)
@@ -71,27 +74,27 @@ int		validate_nbrs(char *raw)
 	i = 0;
 	j = 0;
 	ncount = 0;
+	sharps = 0;
 	while (raw[i] != '\0')
 	{
-		while (j < 16)
+		while ((j < 16) && (raw[i + j] != '\0'))
 		{
 			if (raw[i + j] == '#')
 				sharps++;
 			j++;
 		}
-		if (sharps > 4)
+		if (sharps != 4)
 		{
-			printf("Too many sharps");
+			printf("Wrong number of sharps");
 			return (0);
 		}
 		else if (validate_tetrimino(i, raw) == 0)
 		{
-			printf("not valid");
 			return (0);
 		}
 		sharps = 0;
+		i += j;
 		j = 0;
-		i += 16;
 	}
 	return (1);
 }
