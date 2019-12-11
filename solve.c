9aad3f75fae2838953691b11f40ceeef3746ba23
Tuomas Louekari <tlouekar@c2r4p8.hive.fi>
2019-12-11 12:29:19 +0200

    Continued pseudo solve

--- solve.c
+++ solve.c
@@ -3,7 +3,21 @@
 
 int		solve(char *raw)
 {
-	raw = ft_strnew(0);
+	char	*grid;
+	int		i;
+	int		piece1[5] = {0, 1, 4, 5};
+	int		piece2[5] = {0, 1, 2, 4};
+	int		piece3[5] = {1, 4, 5, 8};
 
+	i = 0;
+	raw = ft_strnew(0);
+	grid = ft_strdup("0011111111111001111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
+	
+
+	while (i < 4)
+	{
+		printf("block values at piecex[%d] in pieces: %d, %d, %d.", i, piece1[i], piece2[i], piece3[i]);
+		i++;
+	}
 	return (0);
 }
