718ef85e235e203ac5064b985c0312d76a28bc97
Melissa Alasalmi <malasalm@c2r4p7.hive.fi>
2019-12-11 14:10:23 +0200

    updates

--- scan_save_tetramino.c
+++ scan_save_tetramino.c
@@ -3,16 +3,17 @@
 #include "fillit.h"
 
 /* scan raw until the first #.
-store the place number of the first # in tetramino string.
+store the place number of the first # in tetcpy string.
 scan the remainder of raw until the next #.
-store the place number of the next # in tetramino string.
+store the place number of the next # in tetcpy string.
 continue until raw reaches the null terminator.
 */
 
 void scan_save_tetramino(char *raw)
 
 {
-	char *tetramino;
+	tetrapak tetramino;
+	char *tetcpy;
 	int i;
 	int j;
 	int hashplace;
@@ -23,12 +24,12 @@ void scan_save_tetramino(char *raw)
 	{
 		if (raw[i] == '#')
 		{
-			tetramino[j] = i;
+			tetcpy[j] = i;
 			j++;
 			i++;
 		}
 		else
 			i++;
 	}
-	ft_strdup(tetramino);
+	tetramino.tetramino = ft_strdup(tetcpy);
 }
