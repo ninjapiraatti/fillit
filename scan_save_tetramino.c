0c545189066c28e4c437ed9a91af52eaf4aee4fb
Melissa Alasalmi <malasalm@c2r4p7.hive.fi>
2019-12-12 18:28:27 +0200

    added play.c where I'm gathering logic for s&s

--- scan_save_tetramino.c
+++ scan_save_tetramino.c
@@ -2,13 +2,6 @@
 #include "libft/includes/libft.h"
 #include "fillit.h"
 
-/* scan raw until the first #.
-store the place number of the first # in tetcpy string.
-scan the remainder of raw until the next #.
-store the place number of the next # in tetcpy string.
-continue until raw reaches the null terminator.
-*/
-
 void	ft_lstaddend(t_list **alst, t_list *new)
 {
 	t_list *lst;
@@ -41,31 +34,42 @@ void make_list(char *tet)
 void scan_save_tetramino(char *raw)
 {
 	char *tet; // string stored in tetpack
+	char *chunk;
+	char *remainder;
+	int size;
 	int i; // raw index
 	int j; // tet index
 	int k; // chunk of raw index
+	int l; // raws chunk index
 
 	i = 0;
 	j = 0;
 	k = 0;
-	while (k < 16) // takes 16 chars from raw then ...
+	l = 0;
+	size = (ft_strlen(raw) / 16); // now we have the number of rounds we want the loop to go
+	while (size != 0)
 	{
-		while (raw[i] != '\0')
+		while (k < 16) // takes 16 chars from raw then ...
+		ft_strncpy(chunk, raw, 16);
+		ft_strncpy(remainder, raw, size - 16);
 		{
-			if (raw[i] == '#') //if there's a #, set the value of tet's char to it's number on the grid.
+			while (raw[i] != '\0')
 			{
-				tet[j] = i;
-				j++;
-				i++;
+				if (raw[i] == '#') //if there's a #, set the value of tet's char to it's number on the grid.
+				{
+					tet[j] = i;
+					j++;
+					i++;
+				}
+				else // otherwise just keep scanning the chunk until a # is found or the chunk ends
+					i++;
 			}
-			else // otherwise just keep scanning the chunk until a # is found or the chunk ends
-				i++;
+			k++;
 		}
-		k++;
+		make_list(tet);
+		size--;
 	}
-	make_list(tet);
-
-	ft_lstaddend(*first, new);
+	// ft_lstaddend(*first, new);
 
 	k = 0;
 	
