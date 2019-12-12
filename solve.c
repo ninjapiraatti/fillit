4ac4eef7c6b85e006efcc467c44260d6fd1540c2
Tuomas Louekari <tlouekar@c2r4p8.hive.fi>
2019-12-12 16:09:47 +0200

    I kind of solves. Well not really.

--- solve.c
+++ solve.c
@@ -1,29 +1,57 @@
 #include <stdio.h> // REMOVE IT YOU DUMBASS
 #include "libft/includes/libft.h"
 
-int		solve(char *raw)
+int		fit_piece(int *piece, char *grid, char piecesign)
 {
-	char	*grid;
 	int		i;
 	int		offset;
 	int		pos;
-	int		piece1[5] = {0, 1, 4, 5};
-	//int		piece2[5] = {0, 1, 2, 4};
-	//int		piece3[5] = {1, 4, 5, 8};
-
+	
 	i = 0;
 	pos = 0;
-	raw = ft_strnew(0);
-	grid = ft_strdup("0011111111111001111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
-	
+	while (i < 4 && pos < 169)
+	{
+		offset = ((piece[i] / 4) * 10);
+		if((grid[piece[i] + offset + pos] - 48) == 0)
+		{
+			//printf("Piece fits.\n");
+			i++;
+		}
+		else
+		{
+			//printf("Piece don't fit.\n");
+			i = 0;
+			pos++;
+		}
+	}
+	i = 0;
 	while (i < 4)
 	{
-		offset = ((piece1[i] / 4) * 9);
-		if((grid[piece1[i] + offset + pos] - 48) == 0)
-			printf("Piece fits.\n");
-		else
-			printf("Piece don't fit.\n");
+		offset = ((piece[i] / 4) * 10);
+		grid[piece[i] + offset + pos] = piecesign;
 		i++;
 	}
+	printf("Grid: \n%s\n", grid);
+	return (1);
+}
+
+int		solve(char *raw)
+{
+	char	*grid;
+	
+	int		piece3[5] = {0, 1, 4, 5};
+	int		piece2[5] = {0, 1, 2, 4};
+	int		piece1[5] = {1, 4, 5, 8};
+	char	piecesign;
+
+	raw = ft_strnew(0);
+	grid = ft_strdup("0000011111111\n0000011111111\n0000011111111\n0000011111111\n0000011111111\n");
+	
+	piecesign = 'A';
+	fit_piece(piece1, grid, piecesign);
+	piecesign = 'B';
+	fit_piece(piece2, grid, piecesign);
+	piecesign = 'C';
+	fit_piece(piece3, grid, piecesign);
 	return (0);
 }
