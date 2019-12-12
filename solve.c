c347f3fbf7708c4c8e1d6e335d687ea67385a929
Tuomas Louekari <tlouekar@c2r4p8.hive.fi>
2019-12-12 14:37:19 +0200

    Solver starts doing something smart

--- solve.c
+++ solve.c
@@ -5,18 +5,24 @@ int		solve(char *raw)
 {
 	char	*grid;
 	int		i;
+	int		offset;
+	int		pos;
 	int		piece1[5] = {0, 1, 4, 5};
-	int		piece2[5] = {0, 1, 2, 4};
-	int		piece3[5] = {1, 4, 5, 8};
+	//int		piece2[5] = {0, 1, 2, 4};
+	//int		piece3[5] = {1, 4, 5, 8};
 
 	i = 0;
+	pos = 0;
 	raw = ft_strnew(0);
 	grid = ft_strdup("0011111111111001111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
 	
-
 	while (i < 4)
 	{
-		printf("block values at piecex[%d] in pieces: %d, %d, %d.", i, piece1[i], piece2[i], piece3[i]);
+		offset = ((piece1[i] / 4) * 9);
+		if((grid[piece1[i] + offset + pos] - 48) == 0)
+			printf("Piece fits.\n");
+		else
+			printf("Piece don't fit.\n");
 		i++;
 	}
 	return (0);
