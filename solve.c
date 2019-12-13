c63a7330bcabe8ad3dea9f4c47ce18548070f36b
Tuomas Louekari <tlouekar@c2r4p8.hive.fi>
2019-12-13 12:43:34 +0200

    Trying to get the tetrimino pieces

--- solve.c
+++ solve.c
@@ -1,57 +1,69 @@
 #include <stdio.h> // REMOVE IT YOU DUMBASS
 #include "libft/includes/libft.h"
+#include "fillit.h"
 
-int		fit_piece(int *piece, char *grid, char piecesign)
+
+int		fit_piece(char *grid, int gridsize)
 {
 	int		i;
 	int		offset;
 	int		pos;
+	int		piece3[5] = {0, 1, 4, 5};
+	int		piece2[5] = {0, 1, 2, 4};
+	int		piece1[5] = {1, 4, 5, 8};
+	int		*temp;
+
+	t_list	*pieces;
 	
+	pieces = ft_lstnew(piece1, 4);
+	ft_lstadd(&pieces, ft_lstnew(piece2, 4));
+	ft_lstadd(&pieces, ft_lstnew(piece3, 4));
 	i = 0;
 	pos = 0;
-	while (i < 4 && pos < 169)
+	offset = 0;
+	temp = pieces->next->next->content;
+	printf("Inside the node: %d\n", temp[0]);
+	/*
+	while (i < 4 && (pos < 169))
 	{
-		offset = ((piece[i] / 4) * 10);
-		if((grid[piece[i] + offset + pos] - 48) == 0)
+		offset = ((pieces->content[i] / 4) * 10);
+		if((grid[pieces->content[i] + offset + pos] - 48) == 0)
 		{
-			//printf("Piece fits.\n");
+			//printf("Pieces->next->content fits.\n");
 			i++;
 		}
 		else
 		{
-			//printf("Piece don't fit.\n");
+			//printf("Pieces->next->content don't fit.\n");
 			i = 0;
 			pos++;
+			if (pos > (offset + gridsize))
+				pos += gridsize - offset;
 		}
 	}
 	i = 0;
 	while (i < 4)
 	{
-		offset = ((piece[i] / 4) * 10);
-		grid[piece[i] + offset + pos] = piecesign;
+		offset = ((pieces->content[i] / 4) * 10);
+		grid[pieces->content[i] + offset + pos] = 'X';
 		i++;
-	}
+	}*/
 	printf("Grid: \n%s\n", grid);
+	printf("Offet: \n%d\n", offset);
+	printf("Gridsize: \n%d\n", gridsize);
 	return (1);
 }
 
 int		solve(char *raw)
 {
 	char	*grid;
-	
-	int		piece3[5] = {0, 1, 4, 5};
-	int		piece2[5] = {0, 1, 2, 4};
-	int		piece1[5] = {1, 4, 5, 8};
-	char	piecesign;
+	int		gridsize;
 
+	gridsize = 5;
 	raw = ft_strnew(0);
 	grid = ft_strdup("0000011111111\n0000011111111\n0000011111111\n0000011111111\n0000011111111\n");
 	
-	piecesign = 'A';
-	fit_piece(piece1, grid, piecesign);
-	piecesign = 'B';
-	fit_piece(piece2, grid, piecesign);
-	piecesign = 'C';
-	fit_piece(piece3, grid, piecesign);
+
+	fit_piece(grid, gridsize);
 	return (0);
 }
