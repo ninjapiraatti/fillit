e5db9b6cab8920b381a6bdb5c5f9d2d98fe2c4e2
Melissa Alasalmi <malasalm@c2r3p7.hive.fi>
2019-12-16 17:29:47 +0200

    updates to s&s

--- scan_save_tetramino.c
+++ scan_save_tetramino.c
@@ -2,7 +2,7 @@
 #include "libft/includes/libft.h"
 #include "fillit.h"
 
-char	*topleft(char *grid)
+char	*topleft(char *chunk)
 {
 	int i;
 	int j;
@@ -11,9 +11,13 @@ char	*topleft(char *grid)
 	tet = ft_memalloc(16);
 	i = 0;
 	j = 0;
-	while (grid[i] != '\0')
+	
+	printf("\ntet after scan for # location:\n");
+	printf("\ntet after move left:\n");
+	printf("\ntet after move up:\n");
+	while (chunk[i] != '\0')
 	{
-		if (grid[i] == 'X')
+		if (chunk[i] == '#')
 		{
 			tet[j] = i;
 			printf("%d ", tet[j]);
@@ -22,18 +26,27 @@ char	*topleft(char *grid)
 		}
 		else 
 			i++;
-		if (grid[0] == 'X' || grid[1] == 'X' || grid[2] == 'X' || grid[3] == 'X')
+		i++;	
+	}
+	
+	if (chunk[0] == '#' || chunk[1] == '#' || chunk[2] == '#' || chunk[3] == '#')
+	{
+		while ((chunk[0] != '#' && chunk[4] != '#' && chunk[8] != '#' && chunk[12] != '#') && (j < 4))
 		{
-			while (grid[0] != 'X' && grid[4] != 'X' && grid[8] != 'X' && grid[12] != 'X')
-			{
-				tet[j] = i - 1;
-				j++;
-			}
+			tet[j] = i - 1;
+			printf("%d ", tet[j]);
+			j++;
+			i++;
 		}
-		while ((grid[0] != 'X' && grid[1] != 'X' && grid[2] != 'X' && grid[3] != 'X') && (j < 4))
+	}
+	if (chunk[0] == '#' || chunk[4] == '#' || chunk[8] == '#' || chunk[12] == '#')
+	{
+		while ((chunk[0] != '#' && chunk[1] != '#' && chunk[2] != '#' && chunk[3] != '#') && (j < 4))
 		{
 			tet[j] = i - 4;
+			printf("%d ", tet[j]);
 			j++;
+			i++;
 		}
 	}
 	return (tet);
@@ -83,35 +96,36 @@ void scan_save_tetramino(char *raw)
 	k = 0;
 	l = 0;
 	tetnum = (ft_strlen(raw) / 16);
-	// printf("tetnum:\n");
-	// printf("%d\n", tetnum);
+	 printf("tetnum:\n");
+	 printf("%d\n", tetnum);
 	while (raw[i] != '\0')
 	{
 		while (j < tetnum)
 		{
 			chunk = ft_strsub(raw, (j * 16), 16);
-			// printf("chunk:\n");
-			// printf("%s\n", chunk);
-			// printf("tet:\n");
-			chunk = topleft(chunk);
+			printf("chunk:\n");
+			printf("%s\n", chunk);
+			printf("tet:\n");
 			while (chunk[k] != '\0')
 			{
 				if (chunk[k] == '#')
 				{
 					tet[l] = k;
-					//printf("%d ", tet[l]);
+					printf("%d ", tet[l]);
 					l++;
 					k++;
 				}
 				else 
 					k++;
-			}
-			// printf("\n");
+			}	
+			if (chunk[0] != '#' || chunk[1] != '#' || chunk[2] != '#' || chunk[3] != '#' || chunk[4] != '#' || chunk[8] != '#' || chunk[12] != '#')
+				chunk = topleft(chunk);
+			printf("\n");
 			k = 0;
 			make_list(tet);
 			l = 0;
 		j++;
-		// printf("\n");
+		 printf("\n");
 		}
 	i++;
 	}
