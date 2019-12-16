d8b8ca8e793cafe2ac7303a7cbca99a6c1af6181
Melissa Alasalmi <malasalm@c2r3p7.hive.fi>
2019-12-16 11:06:35 +0200

    moving ints around a grid is not so simple

---
+++ topleft.c
@@ -0,0 +1,35 @@
+int	topleft(char *grid)
+{
+	int i;
+	int j;
+	int tet[4] = {0, 0, 0, 0};
+
+	i = 0;
+	j = 0;
+	while (grid[i] != '\0')
+	{
+		if (grid[i] == 'X')
+			{
+				tet[j] = i;
+				printf("%d ", tet[j]);
+				j++;
+				i++;
+			}
+			else 
+				i++;
+			}
+		if (grid[0] == 'X' || grid[1] == 'X' || grid[2] == 'X' || grid[3] == 'X')
+		{
+			while (grid[0] != 'X' && grid[4] != 'X' && grid[8] != 'X' && grid[16] != 'X')
+			{
+				tet[j - 4];
+				j++;
+			}
+		}
+		while (grid[0] != 'X' && grid[1] != 'X' && grid[2] != 'X' && grid[3] != 'X')
+		{
+			tet[j - 4];
+			j++;
+		}
+	}
+}
