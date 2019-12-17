47838b077f7de2559552e561926706ea32f5b373
Tuomas Louekari <tlouekar@c1r4p7.hive.fi>
2019-12-17 11:15:16 +0200

    ITS SOLVING ITS SOLVING

--- solve.c
+++ solve.c
@@ -7,17 +7,20 @@ int		recursion(t_list *pieces, int pos, char tletter, char *grid, int gridsize)
 	int			i;
 	int			offset;
 	int			*temp;
+	int			origpos;
 
 	i = 0;
 	offset = 0;
 	if (pieces != NULL)
 	{
 		temp = pieces->content;
-		while (i < 4 && pos < 169)
+		while (i < 4 && pos <= 169)
 		{
 			offset = ((temp[i] / 4) * 10);
 			if((grid[temp[i] + offset + pos] - 48) == 0)
 			{
+				if (i == 0)
+					origpos = pos;
 				//printf("Piece fits.\n");
 				//printf("Grid: \n%s\n", grid);
 				i++;
@@ -27,28 +30,46 @@ int		recursion(t_list *pieces, int pos, char tletter, char *grid, int gridsize)
 				//printf("Piece don't fit.\n");
 				i = 0;
 				pos++;
-				if (pos > (offset + gridsize))
-					pos += gridsize - offset;
+				// if (pos > (offset + gridsize))
+				// 	pos += gridsize - offset;
 			}
+			//printf("Pos: \n%d\n", pos);
 		}
+		//printf("Origpos: %d, Pos: %d\nGrid: \n%s\n", origpos, pos, grid);
+		//getchar();
 		if (pos > 169)
-		{
-			printf("Failed to fit the piece");
+		{	
+			//printf("Failed to fit the piece");
 			return (0);
 		}
 		i = 0;
 		while (i < 4)
 		{
 			offset = ((temp[i] / 4) * 10);
+			//printf("Writing");
 			//printf("temp[i], offset, pos: %d, %d, %d\n", temp[i], offset, pos);
 			grid[temp[i] + offset + pos] = tletter;
 			i++;
 		}
-		//pieces = pieces->next;
-		return (1);
+		if (recursion(pieces->next, 0, ++tletter, grid, gridsize) == 0)
+		{
+			//printf("Grid: \n%s\n", grid);
+			i = 0;
+			while (i < 4)
+			{
+				offset = ((temp[i] / 4) * 10);
+				//printf("temp[i], offset, pos: %d, %d, %d\n", temp[i], offset, pos);
+				//printf("Erasing");
+				grid[temp[i] + offset + pos] = '0';
+				i++;
+			}
+			recursion(pieces, ++origpos, --tletter, grid, gridsize);
+		}
+		else
+			return (1);
+		return (0);
 	}
-	//recursion(pieces, pos++, tletter, grid, gridsize);
-	return (0);
+	return (1);
 }
 
 int		fit_piece(char *grid, int gridsize)
@@ -60,8 +81,8 @@ int		fit_piece(char *grid, int gridsize)
 	int			piece2[4] = {0, 1, 2, 4};
 	int			piece3[4] = {1, 4, 5, 8};
 	int			piece4[4] = {0, 1, 4, 5};
-	int			piece5[4] = {0, 1, 2, 4};
-	int			piece6[4] = {0, 1, 4, 5};
+	int			piece5[4] = {0, 1, 4, 5};
+	//nt			piece6[4] = {0, 1, 2, 3};
 	char		tletter;
 	int			i;
 
@@ -71,24 +92,21 @@ int		fit_piece(char *grid, int gridsize)
 	ft_lstadd(&pieces, ft_lstnew(piece3, 16));
 	ft_lstadd(&pieces, ft_lstnew(piece4, 16));
 	ft_lstadd(&pieces, ft_lstnew(piece5, 16));
-	ft_lstadd(&pieces, ft_lstnew(piece6, 16));
+	//ft_lstadd(&pieces, ft_lstnew(piece6, 16));
 	pos = 0;
 	tletter = 'A';
-	while (grid[i] != '\0')
+	if (recursion(pieces, pos, tletter, grid, gridsize) == 1)
 	{
-		if (recursion(pieces, pos, tletter, grid, gridsize) == 1)
-		{
-			pieces = pieces->next;
-			tletter++;
-			printf("Grid: \n%s\n", grid);
-			//recursion(pieces->next, pos, tletter++, grid, gridsize);
-		}
-		else
-		{
-			// reset the board, start over from pos+1?
-			pos++;
-		}
-		i++;
+		//pieces = pieces->next;
+		//tletter++;
+		printf("Return 1. Grid: \n%s\n", grid);
+		//recursion(pieces->next, pos, tletter++, grid, gridsize);
+	}
+	else
+	{
+		printf("Return 0. Grid: \n%s\n", grid);
+		// reset the board, start over from pos+1?
+		pos++;
 	}
 	//printf("Inside the node: %d, %d, %d, %d\n", &pieces->next->content[0], &pieces->next->content[1], &pieces->next->content[2], &pieces->next->content[3]);
 	//printf("Inside the node: %d, %d, %d, %d\n", temp[0], temp[1], temp[2], temp[3]);
