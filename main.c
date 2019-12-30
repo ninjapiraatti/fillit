6b6fa46f139a20ead4a6e0d203d9bbc4c5e33763
Melissa Alasalmi <malasalm@c2r4p7.hive.fi>
2019-12-30 09:57:28 +0200

    added executable name to makefile

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "libft/includes/libft.h"
 #include "fillit.h"
 #include <fcntl.h>
+#include <stdio.h>
 
 int		main(int argc, char **argv)
 {
@@ -8,11 +9,11 @@ int		main(int argc, char **argv)
 	char	*raw;
 	int		fd;
 	t_list	*pieces;
-	int		i;
+	// int i;
 
-	i = 0;
 	pieces = NULL;
 	fd = 0;
+	// i = 0;
 	if (argc != 2)
 	{
 		ft_putstr("usage: fillit <filename>.\n");
@@ -20,23 +21,15 @@ int		main(int argc, char **argv)
 	}
 	fd = open(argv[1], O_RDONLY);
 	raw = ft_strnew(0);
+
 	while (get_next_line(fd, &line) == 1)
 	{
 		if (ft_strlen(line) > 5)
 			break ;
 		raw = ft_strjoin(raw, line);
-		i++;
-		//ft_putnbr(i);
-		if (i % 5 == 0)
-		{
-			if (validate(raw) == 0)
-			{
-				//ft_putstr("Error on linebreaks.");
-				return (0);
-			}
-			i = 0;
-		}
 	}
+	if (validate(raw) == 0)
+		return (0);
 	solve(scan_save_tetramino(raw, pieces));
 	return (0);
 }
