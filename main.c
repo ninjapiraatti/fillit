5352168e8c4a28f7c0a80aff29dc4403d3afcbfe
Tuomas Louekari <tlouekar@c2r4p8.hive.fi>
2019-12-30 09:21:23 +0200

    Fighting to get the last error checks right

--- main.c
+++ main.c
@@ -18,25 +18,38 @@ int		main(int argc, char **argv)
 		ft_putstr("usage: fillit <filename>.\n");
 		return (0);
 	}
-	fd = open(argv[1], O_RDONLY);
+	if ((fd = open(argv[1], O_RDONLY)) != 3)
+		return (0);
 	raw = ft_strnew(0);
 	while (get_next_line(fd, &line) == 1)
 	{
 		if (ft_strlen(line) > 5)
-			break ;
+			return (0);
 		raw = ft_strjoin(raw, line);
 		i++;
 		//ft_putnbr(i);
+		/*
+		if (validate(raw) == 0)
+		{
+			ft_putstr("Error somewhere.");
+			return (0);
+		}
+		*/
 		if (i % 5 == 0)
 		{
 			if (validate(raw) == 0)
 			{
-				//ft_putstr("Error on linebreaks.");
+				ft_putstr("Error somewhere.");
 				return (0);
 			}
 			i = 0;
 		}
 	}
+	if (validate(raw) == 0)
+		{
+			ft_putstr("Error somewhere.");
+			return (0);
+		}
 	solve(scan_save_tetramino(raw, pieces));
 	return (0);
 }
