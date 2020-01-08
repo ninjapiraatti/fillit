8d8c129188c2985295a91c1e33c7d21c40c84b20
Ninjapiraatti <tuoppil@gmail.com>
2020-01-08 22:19:30 +0200

    No leaks

--- fillitsrcs/print_grid.c
+++ fillitsrcs/print_grid.c
@@ -6,27 +6,51 @@
 /*   By: tlouekar <tlouekar@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/01 13:13:45 by tlouekar          #+#    #+#             */
-/*   Updated: 2020/01/08 17:11:43 by tlouekar         ###   ########.fr       */
+/*   Updated: 2020/01/08 22:18:29 by tlouekar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/includes/libft.h"
+#include <stdio.h>
 
-void	ft_lstdelfuck(t_list *alst, void (*del)(void *, size_t))
+void	ft_lstdelfuck(t_list **alst, void (*del)(void *, size_t))
 {
 	t_list	*list;
 	t_list	*next;
+	int		i;
 
-	list = alst;
+	i = 0;
+	list = *alst;
 	while (list)
 	{
+		i++;
 		next = list->next;
 		del(list->content, list->content_size);
 		free(list);
 		list = NULL;
 		list = next;
+		ft_putnbr(i);
 	}
-	alst = NULL;
+	*alst = NULL;
+}
+
+void	ft_lstdelfuck2(t_list *pieces)
+{
+	t_list	*list;
+	t_list	*next;
+	int		i;
+
+	i = 0;
+	list = pieces;
+	while (list)
+	{
+		i++;
+		next = list->next;
+		free(list);
+		list = NULL;
+		list = next;
+	}
+	pieces = NULL;
 }
 
 void	print_grid(t_list *pieces, char *grid)
@@ -45,8 +69,16 @@ void	print_grid(t_list *pieces, char *grid)
 		ft_putchar(grid[i]);
 		i++;
 	}
-	free(pieces);
+	/*
+	while (pieces)
+	{
+
+	}
+	*/
+	//free(pieces);
 	//free(pieces->next);
-	ft_lstdelfuck(pieces, ft_bzero);
+	//free(pieces->next->next);
+	//ft_lstdelfuck(&pieces, ft_bzero);
+	ft_lstdelfuck2(pieces);
 	free(grid);
 }
